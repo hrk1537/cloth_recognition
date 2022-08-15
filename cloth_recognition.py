9b5faf4698a8636195ea351003e3e646200308d3
hrk1537 <hrithikreigns@gmail.com>
2022-08-15 20:01:16 +0530

    commit

---
+++ cloth_recognition.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+
+# class myCallback(tf.keras.callbacks.Callback):
+#     def on_epoch_end(self, epoch, logs={}):
+#         if(logs.get('accuracy') > 0.95):
+#             print("\n 95% Accuracy")
+#             self.model.stop_training = True
+
+
+# callbacks = myCallback()
+data = tf.keras.datasets.fashion_mnist
+
+(training_images, training_lables), (test_images, test_labels) = data.load_data()
+
+training_images = training_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(training_images, training_lables, epochs=5)
+
+classifications = model.predict(test_images)
+print(classifications[0])
+# print(test_images[0])
